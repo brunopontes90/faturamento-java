87e06e78d594cb4db0751adba8873412664df921
Bruno Pontes <bruno_confessor@hotmail.com>
2022-12-26 13:04:45 -0300

    Adicioando arquios Java

---
+++ src/main/java/com/mycompany/faturamento/java/FaturamentoJava.java
@@ -0,0 +1,9 @@
+
+package com.mycompany.faturamento.java;
+
+public class FaturamentoJava {
+
+    public static void main(String[] args) {
+        System.out.println("Iniciando o projeto!");
+    }
+}
