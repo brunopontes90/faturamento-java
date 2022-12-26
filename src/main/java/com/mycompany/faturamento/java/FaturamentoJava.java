8a52f797cc4a4304916ad1b7ca72b74a9a303eb9
Bruno Pontes <bruno_confessor@hotmail.com>
2022-12-26 13:40:02 -0300

    Montando a Estrutura

--- src/main/java/com/mycompany/faturamento/java/FaturamentoJava.java
+++ src/main/java/com/mycompany/faturamento/java/FaturamentoJava.java
@@ -1,9 +1,25 @@
 
 package com.mycompany.faturamento.java;
 
+import java.util.Scanner;
+
 public class FaturamentoJava {
 
     public static void main(String[] args) {
-        System.out.println("Iniciando o projeto!");
+        System.out.println("********** SISTEMA DE FATURAMENTO **********");
+        Scanner scanner = new Scanner(System.in);
+              
+        System.out.print("Nome do produto: ");
+        String produto = scanner.next();
+        
+        System.out.print("Preço do Produto: ");
+        float preco = scanner.nextFloat();
+        
+        System.out.print("Unidades: ");
+        int qtdUnidade = scanner.nextInt();
+        
+        float valorTotal = preco * qtdUnidade;
+        System.out.print("Valor Total a Pagar: " + valorTotal);
+        
     }
 }
